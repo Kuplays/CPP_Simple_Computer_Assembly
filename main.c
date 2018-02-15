bb99db3818b884373fdf0e89770819645e3503c1
Aleksandr Kuplays <alexkuplays@gmail.com>
2018-02-15 23:56:29 +0700

    Mem arr tests

--- main.c
+++ main.c
@@ -5,6 +5,11 @@
 int main() {
 	sc_memoryInit();
 	int i;
+	printf("\nMEM ARR TEST: \n");
+	for (i = 0; i < MEM_COUNT; i++) {
+		printf("%X", memArr[i]);
+	}
+	printf("COM ARR TEST:\n");
 	for (i = 0; i < COM_COUNT; i++) {
 		printf("%d ", comArr[i]);
 	}
