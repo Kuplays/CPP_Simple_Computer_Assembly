dbf1d6e8e5daeaa54e4022b2deca2058dff75ecd
Aleksandr Kuplays <alexkuplays@gmail.com>
2018-03-09 20:39:53 +0700

    All funcs added

--- myBigChars.c
+++ myBigChars.c
@@ -66,6 +66,54 @@ int bc_printBigChar(int big[2], int x, int y, enum Color fgColor, enum Color bgC
 			bc_printA(str);
 		}
 	}
+
+	return 0;
+}
+
+int bc_setBigCharPos(int *big, int x, int y, int value) {
+
+	if (x < 0 || x > 7 || y < 0 || y > 7 || value > 1 || value < 0) return -1;
 	
+	int index = 0;
+
+	if (x > 3) index = 1;
+
+
+	if (value == 1) big[index] |= 1 << (x * 8 + y);
+	else if (value == 0) big[index] &= ~(1 << (x * 8 + y));
+	
+	return 0;
+}
+
+int bc_getBigCharPos(int *big, int x, int y, int *value) {
+	if (x < 0 || x > 7 || y < 0 || y > 7) return -1;
+
+	int index = 0;
+	if (x > 3) index = 1;
+
+	*value = ((big[index] >> (x * 8)) >> y) & 1;
+
+	return 0;
+}
+
+int bc_bigCharWrite(int fd, int *big, int count) {
+	int result;
+	while (count > 0) {
+		if ((result = write(fd, big, sizeof(int) * 2)) == -1) return -1;
+		count--;
+	}
+
+	return 0;
+}
+
+int bc_bigCharRead(int fd, int *big, int need_count, int *count) {
+	while (need_count > 0) {
+		int result;
+		if ((result = read(fd, big, sizeof(int) * 2)) == -1) return -1;
+		
+		need_count--;
+		*count = *count + 1;
+	}
+
 	return 0;
 }
