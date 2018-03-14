80fc03143c6bacf284dbd79ef3c3e463cfc2c038
Aleksandr Kuplays <alexkuplays@gmail.com>
2018-03-15 03:31:54 +0700

    MyReadKey lib

--- myReadKey.h
+++ myReadKey.h
@@ -1,8 +1,12 @@
 #ifndef MYREADKEY_H
 #define MYREADKEY_H
 
-int rk_readKey(enum Keys *);
-int rk_myTermSave();
-int rk_myTermRestore();
-int rk_myTermRegime(int, int, int, int, int);
+enum Keys {UP, DOWN,LEFT, RIGHT, F5, F6, ESC, ENTER, LOAD, SAVE, RUN, STEP, RESET, QUIT, EDIT, UNKNOWN};
+
+void termInit();
+void defaultTermSettings(struct termios *);
+int rk_readKey(enum Keys *key);
+int rk_myTermSave(struct termios *);
+int rk_myTermRestore(struct termios *);
+int rk_myTermRegime(struct termios const*, int, int, int, int, int);
 #endif
